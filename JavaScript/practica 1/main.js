449fcf384a37f9314f3485e18056f13b30e5667b
AtomHeartQueer <albertvaladopujol@gmail.com>
2023-01-19 14:57:53 +0100

    práctica 1 JavaScript

---
+++ JavaScript/practica 1/main.js
@@ -0,0 +1,18 @@
+let carName = 'Volvo';
+console.log(carName);
+
+let x = 50;
+console.log(x);
+
+x = 5;
+let y = 10;
+
+document.write(x+y);
+
+let z = x+y;
+alert(z);
+
+let firstName='Jon';
+let lastName='Snow';
+let age=24;
+console.log('Soy '+firstName+' '+lastName+' , tengo '+age+' años y me gustan los lobos.');
