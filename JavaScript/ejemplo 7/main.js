7315b16b9679aa3424823208a93177f0f17ee18b
AtomHeartQueer <albertvaladopujol@gmail.com>
2023-02-02 10:18:55 +0100

    Ejemplo 7

---
+++ JavaScript/ejemplo 7/main.js
@@ -0,0 +1,16 @@
+//Template Literals
+const edad = 33;
+const str = `Hola, tengo ${edad} años`;
+
+//También permiten meter saltos de línea
+const divNodeText = `
+    <div>
+        <p>Hola, chicos!<p>
+    <div>
+`;
+
+//Arrow functions
+//permiten declarar funciones de manera más compacta, así como lidiar mejor con el scope.
+//pueden ser anónimas o asignarse a una variable
+
+const a = () => {};
