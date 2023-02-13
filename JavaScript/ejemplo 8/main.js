0d2e348a584b6624eb12f4ccc4adf99b345ab3f3
AtomHeartQueer <albertvaladopujol@gmail.com>
2023-02-13 11:45:45 +0100

    filter, find, reduce

--- JavaScript/ejemplo 8/main.js
+++ JavaScript/ejemplo 8/main.js
@@ -162,3 +162,116 @@ const empireBooleanAnimals = animalsdAigo.map((animal) => {
 });
 
 console.log(empireBooleanAnimals);
+
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90, 18];
+
+const older = ages.filter((age) => {
+  return age >= 18;
+});
+
+console.log(older);
+
+//----------------------------------------- .find() -------------------------------
+//devuelve el valor del primer elemento que cumple la función de prueba proporcionada
+//a diferencia de filter que siempre devuelve un array aunque sea de una sola posición
+
+const array1 = [5, 12, 20, 8, 25, 130];
+
+const found = array1.find((element) => element > 10);
+console.log(found); //devuelve 12
+
+const aliens = [
+  { name: "Zalameto", planet: "Eden", age: 4029 },
+  { name: "Paktu", planet: "Andromeda", age: 32 },
+  { name: "Cucushumusu", planet: "Marte", age: 5023021 },
+];
+
+const mutations = [
+  { name: "Porompompero", description: "Permite tocar el tambor" },
+  {
+    name: "Fly me to the moon",
+    description: "Permite volar, pero solo a la luna",
+  },
+  {
+    name: "Andando que es gerundio",
+    description: "Invoca a un señor mayor como Personal Trainer",
+  },
+];
+
+const foundAlien = aliens.find((alien) => alien.name === "Cucushumusu");
+const foundMutation = mutations.find(
+  (mutation) => mutation.name === "Porompompero"
+);
+
+const mutatedAlien = { ...foundAlien, mutation: { ...foundMutation } };
+console.log(mutatedAlien);
+
+//----------------------------------------- .reduce() -------------------------------
+const mesAnimalsdAigo = [
+  { id: 1, name: "Albert", years: 26 },
+  { id: 2, name: "Mangel", years: 28 },
+  { id: 3, name: "Toforu", years: 26 },
+  { id: 4, name: "Andreu", years: 28 },
+];
+
+const totalYears = mesAnimalsdAigo.reduce((accumulator, pilot) => {
+  return accumulator + pilot.years;
+}, 0);
+
+console.log(totalYears);
+
+const exam = [
+  { name: "Paquita Salas", score: 5 },
+  { name: "Pepe Palotes", score: 3 },
+  { name: "Fulanito Pascual", score: 8 },
+  { name: "Menganito de Tal", score: 7 },
+  { name: "Teresa Gonzalez", score: 4 },
+  { name: "Pedro Pascal", score: 7 },
+  { name: "Lluc Garau", score: 10 },
+  { name: "darkdeathy69", score: 2 },
+];
+
+const totalScore = exam.reduce((accum, alumn) => {
+  return accum + alumn.score;
+}, 0);
+
+console.log(totalScore);
+
+const approvedScore = exam.reduce((accum, alumn) => {
+  return alumn.score >= 5 ? accum + alumn.score : accum;
+}, 0);
+
+console.log(approvedScore);
+
+const averageExamScore = exam.reduce((accum, alumn) => {
+  return accum + alumn.score / exam.length;
+}, 0);
+
+const averageExam = totalScore / exam.length;
+
+console.log(averageExamScore, averageExam); //mismo resultado de dos maneras distintas
+
+/**const videogames = [
+  { name: "Final Fantasy VII", genre: ["RPG"], score: 9.5 },
+  { name: "AC Valhalla", genre: ["Aventura", "RPG"], score: 4.5 },
+  { name: "The Last of Us 2", genre: ["Accion", "Aventura"], score: 9.8 },
+  { name: "Super Mario Bros.", genre: ["Plataforma"], score: 8.5 },
+  { name: "Genshin Impact", genre: ["RPG", "Aventura"], score: 7.5 },
+  {
+    name: "Breath of the Wild",
+    genre: ["RPG", "Masterpiece"],
+    score: 10,
+  },
+];
+
+const vdgRPG = videogames.filter((game) => {
+  game.genre.forEach((element) => {
+    return element === "RPG";
+  });
+});
+
+const vdgRPGAverage = vdgRPG.reduce((accum, game) => {
+  return accum + game.score / vdgRPG.length;
+});
+
+console.log(vdgRPGAverage); **/
