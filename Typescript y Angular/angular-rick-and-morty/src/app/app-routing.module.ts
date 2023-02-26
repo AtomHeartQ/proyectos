701ed364df890945f620a8446099d4fc62f0d1ba
AtomHeartQueer <albertvaladopujol@gmail.com>
2023-02-26 15:41:03 +0100

    creación proyecto e integración bootstrap

---
+++ Typescript y Angular/angular-rick-and-morty/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CharactersPageComponent } from './pages/characters-page/characters-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+const routes: Routes = [
+  {path:'', component: HomePageComponent},
+  {path: 'characters', component:CharactersPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Typescript y Angular/angular-rick-and-morty/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MenuComponent } from './core/components/menu/menu.component';
+import { CharactersPageComponent } from './pages/characters-page/characters-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GalleryComponent } from './shared/components/gallery/gallery.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    MenuComponent,
+    CharactersPageComponent,
+    GalleryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
