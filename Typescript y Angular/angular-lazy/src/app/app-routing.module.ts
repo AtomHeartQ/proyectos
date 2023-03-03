f5b439391926598c10256988a49dda5a4014b3f8
AtomHeartQueer <albertvaladopujol@gmail.com>
2023-03-03 16:48:15 +0100

    angular lazy project creation

---
+++ Typescript y Angular/angular-lazy/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '', loadChildren: () =>
+      import('./pages/home-page/home-page.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'contact', loadChildren: () =>
+      import('./pages/contact-page/contact-page.module').then(m => m.ContactPageModule)
+  },
+  {
+    path: 'about', loadChildren: () =>
+      import('./pages/about-page/about-page.module').then(m => m.AboutPageModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Typescript y Angular/angular-lazy/src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './components/button/button.component';
+import { HeadingComponent } from './components/heading/heading.component';
+
+
+
+@NgModule({
+  declarations: [
+    ButtonComponent,
+    HeadingComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [ButtonComponent, HeadingComponent]
+})
+export class SharedModule { }
